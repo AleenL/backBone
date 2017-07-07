c479bda2c92f9678aa09ba4d9a45666330ef76a3
aleenL <784757555@qq.com>
2017-07-07 13:58:32 +0800

    state是什么？？

--- react-demo/src/Welcome.js
+++ react-demo/src/Welcome.js
@@ -2,8 +2,24 @@ import React from 'react';
 import './Welcome.css'
 
 
-function Welcome(props) {
-    return <h1 > hello, { props.width } < /h1>
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props)
+        this.Setstate = {
+            data: new Date()
+        }
+    }
+    render() {
+        return ( <
+            div >
+            <
+            h1 > hello, { this.props.name } < /h1> <
+            h2 > { this.Setstate.data.toString() } < /h2>
+
+            <
+            /div>
+        )
+    }
 }
 
 export default Welcome
