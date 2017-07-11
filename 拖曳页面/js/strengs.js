2604733e109d579d26776b299e3d23a705416f0e
aleenL <784757555@qq.com>
2017-07-11 20:30:54 +0800

    绑定bind

--- 拖曳页面/js/strengs.js
+++ 拖曳页面/js/strengs.js
@@ -6,7 +6,19 @@ var Animate = (function() {
 	}
 	
 	_Animate.prototype.init = function(){
-		var $foldingBtn = this.$foldingBtn = $ct.find('.folding_page')
+		var $foldingBtn = this.$foldingBtn =  this.$ct.find('.folding_page'),
+			$ulPage = this.$ulPage =  this.$ct.find('.content_inside_items_children'),
+			$addChild = this.$addChild =  this.$ct.find('.add_li'),
+			$addParent = this.$addParent =  this.$ct.find('.add_new_parent'),
+			$parentUlPage = this.$parentUlPage =  this.$ct.find('.content_inside_items')
+	}
+	
+	_Animate.prototype.bind = function(){
+		this.$addChild.on('click', function(e) {
+				var childIndex = this.$addChild.index(this);
+				newLi = "<li><p></p><p class='video_link add_link_color'>+关联视频</p></li>"
+				$(this).before(newLi)
+			})
 	}
 	
 	return {
