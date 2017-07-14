9da98c25a4c49e22246ebd9da12a1d094777f3f0
aleenL <784757555@qq.com>
2017-07-14 19:33:34 +0800

    ajax

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+	Component
+} from 'react';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
@@ -6,29 +8,48 @@ import 'normalize.css';
 import GetCity from './getCity';
 import $ from 'jquery'
 
- class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            newTodo: '',
-            todoList: [],
-            city: " ",
-        }
-    }
-    render() {
-        let todos = this.state.todoList
-        	.filter((item) => !item.deleted) //过滤掉deleted为ture的值，留下不等于true的值
-        	.map((item, index) =>{//遍历数组todoList中的值
-        	return (<li key={index}>
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			newTodo: '',
+			todoList: [],
+			city: "dad ",
+		}
+	}
+
+	componentDidMount() {
+		console.log(1)
+		let city = this.state.city
+		var that = this
+		let cityUrl = "https://api.map.baidu.com/location/ip?ak=O6SDTtgTtymLR2UzklNO9eYolcZLXI7Q";
+		$.ajax({
+			url: cityUrl,
+			type: 'get',
+			async: false,
+			dataType: 'jsonp',
+			success: function(data) {
+				that.setState({
+					city: data.content.address
+				})
+			}
+		})
+	}
+
+	render() {
+		let todos = this.state.todoList
+			.filter((item) => !item.deleted) //过滤掉deleted为ture的值，留下不等于true的值
+			.map((item, index) => { //遍历数组todoList中的值
+				return(<li key={index}>
 						<TodoItem todo={ item } /*定义todo为this.state.todoList中值*/ 
 						onToggle = {this.toggle.bind(this)} 
 						onDelete = {this.delete.bind(this)}/>
-            		</li>)     
-        })
-        
-        return ( 
-        	<div className = "App" >
-            	<GetCity title = {this.getCity()}/>
+            		</li>)
+			})
+
+		return(
+			<div className = "App" >
+            	<GetCity title = {this.state.city}/>
             	<div className = 'inputWrapper'>
             		<TodoInput content = { this.state.newTodo } /*定义content为this.state.newTodo*/ 
             		onChange = {this.changeTitle.bind(this)} 
@@ -37,63 +58,47 @@ import $ from 'jquery'
             	</div>
             	<ol> { todos } </ol> 
             </div >
-        )
-    }
-    
+		)
+	}
 
-	getCity() {
-    	let cityUrl = "https://api.map.baidu.com/location/ip?ak=O6SDTtgTtymLR2UzklNO9eYolcZLXI7Q";
-        $.ajax({
-            url:cityUrl,
-            type:'get',
-            async: false,
-            dataType: 'jsonp',
-            success: function(data){
-            	return 1
-               }
-          })
-    }
-    
+	delete(event, todo) {
+		todo.deleted = true
+		this.setState(this.state) //重置了state的值
 
-    delete(event, todo){
-    	todo.deleted = true
-    	this.setState(this.state)//重置了state的值
+	}
 
-    }
-    
-    
-    toggle(e,todo){
-    	todo.status = todo.status === 'completed' ? '' : 'completed' //判断todo.states是否等于completed，
-    	this.setState(this.state)
-    }
-    
-    changeTitle(event){
-    	this.setState({
-    		newTodo: event.target.value,
-    		todoList: this.state.todoList
-    	})
-    }
-	
-    addTodo(event){
-    	this.state.todoList.push({
-    		id: idMarker(),
-    		title: event.target.value,
-    		status: 'null',
-    		deleted: false
-    	})
-    	
-    	this.setState({
-    		newTodo: '',
-    		todoList: this.state.todoList
-    	})
-    }
+	toggle(e, todo) {
+		todo.status = todo.status === 'completed' ? '' : 'completed' //判断todo.states是否等于completed，
+		this.setState(this.state)
+	}
+
+	changeTitle(event) {
+		this.setState({
+			newTodo: event.target.value,
+			todoList: this.state.todoList
+		})
+	}
+
+	addTodo(event) {
+		this.state.todoList.push({
+			id: idMarker(),
+			title: event.target.value,
+			status: 'null',
+			deleted: false
+		})
+
+		this.setState({
+			newTodo: '',
+			todoList: this.state.todoList
+		})
+	}
 }
 
 export default App;
 
 let id = 0
 
-function idMarker(){
+function idMarker() {
 	id += 1
 	return id
 }
--- react-demo/src/getCity.js
+++ react-demo/src/getCity.js
@@ -5,8 +5,5 @@ export default class GetCity extends Component {
 	render(){
 		return <h2> {this.props.title} </h2> 
 	}
-	
-	componentWillUpdate(){
-		console.log(111)
-	}
+
 }
